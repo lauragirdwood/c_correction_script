e379b0ce5df0dae252b07202c8cf5c5f359e3495
pyago-ra <pyago-ra@student.42sp.org.br>
2021-04-06 18:16:26 +0000

    Updated

--- C_02_Test/ex12/compile.sh
+++ C_02_Test/ex12/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+clear;
 gcc *.c -o ex11_t -Wall -Wextra -Werror;
-./ex11_t > result_teste;
-diff compare_teste result_teste > teste.diff;
+./ex11_t;
+echo "-------------------------------------";
+./ex11_t | cat -e;
+
+echo "O endereço de memoria é diferente, então veja o arquivo .compare_teste para verificar a saida."
--- C_02_Test/ex12/main.c
+++ C_02_Test/ex12/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 
-void *ft_print_memory(void *addr, unsigned int size);
+void	*ft_print_memory(void *addr, unsigned int size, unsigned int index);
 
 int main()
 {
 	char *teste = "Bonjour les aminches\t\n\tc  est fou\ttout\tce qu on peut faire avec\t\n\tprint_memory\n\n\n\tlol.lol\n ";
-	ft_print_memory(teste, 92);
+	ft_print_memory(teste, 92, 0);
 	return (0);
 }
