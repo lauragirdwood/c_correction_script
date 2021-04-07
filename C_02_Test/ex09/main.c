32bf951cc48acaf6c02ae04f62076e81e65e9db7
pyago-ra <pyago-ra@student.42sp.org.br>
2021-04-07 15:50:35 +0000

    C_02 ex09 modificado para abranger mais opções

--- C_02_Test/ex09/main.c
+++ C_02_Test/ex09/main.c
@@ -5,7 +5,9 @@ char *ft_strcapitalize(char *str);
 
 int main()
 {
-	char str[] = "oi, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
-	printf("%s\n", ft_strcapitalize(str));
+	char str1[] = "oi, tudo bem? 42palavras quarenta-e-duas; cinquenta+e+um";
+	char str2[] = "oi, TuDo BeM? 42PaLaVrAs QuArEnTa-E-dUAs; cinquentA+e+uM";
+	printf("%s\n", ft_strcapitalize(str1));
+	printf("%s\n", ft_strcapitalize(str2));
 	return (0);
 }
--- C_03_Test/ex00/main.c
+++ C_03_Test/ex00/main.c
@@ -5,11 +5,22 @@ int ft_strcmp(char *s1, char *s2);
 
 int main()
 {
-	printf("%d\n", strcmp("abc", "ABC"));
-	printf("%d\n", ft_strcmp("abc", "ABC"));
-	printf("%d\n", strcmp("abc", "abc"));
-	printf("%d\n", ft_strcmp("abc", "abc"));
-	printf("%d\n", strcmp("ABC", "abc"));
-	printf("%d\n", ft_strcmp("ABC", "abc"));
+	char *str1 = "a";
+	char *str2 = "A";
+	char *str3 = "A";
+	char *str4 = "a";
+	char *str5 = "b";
+	char *str6 = "a";
+	char *str7 = "a";
+	char *str8 = "b";
+
+	printf("%d\n", strcmp("a", "A"));
+	printf("%d\n", strcmp("A", "a"));
+	printf("----------------------------\n");
+	printf("%d\n", strcmp(str1, str2));
+	printf("%d\n", strcmp(str3, str4));
+	printf("----------------------------\n");
+	printf("%d\n", strcmp(str5, str6));
+	printf("%d\n", strcmp(str7, str8));
 	return (0);
 }
